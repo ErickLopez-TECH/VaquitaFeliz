6cf9936dbe60af78d8f37bbb592a1f369bee90db
Erick <ericklopezborge21@gmail.com>
2026-06-24 15:49:47 -0600

    50% menu principal Actualizacion

--- VaquitaFeliz/src/Logica/VaquitaFeliz.java
+++ VaquitaFeliz/src/Logica/VaquitaFeliz.java
@@ -4,6 +4,8 @@
  */
 package Logica;
 
+import java.util.Scanner;
+
 /**
  *
  * @author triamus
@@ -13,9 +15,61 @@ public class VaquitaFeliz {
     /**
      * @param args the command line arguments
      */
+    /*-------------------------------------------/
+    |            VARIABLES GLOBALES|
+    ------------------------------------------  */
+    private static Scanner leer = new Scanner(System.in);
+    
+    /*-------------------------------------------/
+    | MENU PRINCIPAL,GESTION DE TODOS LOS MODULOS|
+    ------------------------------------------  */
+    public static void menuPrincipal(){
+        int opciones= 0;
+        
+        do {            
+            System.out.println("-----------------------------------------");
+            System.out.println("|             VAQUITA FELIZ             |");
+            System.out.println("-----------------------------------------");
+            System.out.println("Ingrese una opcion(1-5), luego presione");
+            System.out.println("la tecla enter.");
+            System.out.println("1.Gestion de vacas");
+            System.out.println("2.Gestion de alimentos");
+            System.out.println("3.Registro de alimentacion");
+            System.out.println("4.Registro de produccion");
+            System.out.println("5.Reportes produccion/Alimentacion");
+            System.out.println("6.Cerrar Sistema");
+            opciones = leer.nextInt();
+            
+            
+            //Estructura de control y decision
+            switch(opciones){
+                case 1: 
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+            }
+            if(opciones ==6){
+                System.out.print(0);
+            }
+            
+            if((opciones >6) || (opciones <1)){
+                System.out.println("-----------------------------------------");
+                System.out.println("|             OPCION INCORRECTA         |");
+                System.out.println("-----------------------------------------");
+                menuPrincipal();
+            }
+        } while (opciones <6 );
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
-        MenuVaquitas.menuVaquitas();
+        menuPrincipal();
     }
     
 }
