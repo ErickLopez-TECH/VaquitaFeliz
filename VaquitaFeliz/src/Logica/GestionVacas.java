debded3b2bbaf32332584130ce297e19afd27309
Erick <ericklopezborge21@gmail.com>
2026-06-24 19:11:45 -0600

    Nuevos metodos

--- VaquitaFeliz/src/Logica/GestionVacas.java
+++ VaquitaFeliz/src/Logica/GestionVacas.java
@@ -18,6 +18,7 @@ public class GestionVacas {
     
     static int[] codigoVaca = new int[100];
     static String[] nombreVaca = new String[100];
+    static String[] nombreVacas = new String[100];
     static int[] edadVaca = new int[100];
     static float[] pesoVaca = new float[100];
     static String[] estadoProductivo = new String[100];
@@ -45,14 +46,14 @@ public class GestionVacas {
             opcion = leer.nextInt();
             
             switch (opcion) {
-                case 1:
+                case 1:registrarVacas();
                     
                     break;
                 case 2:
                     break;
                 case 3: 
                     break;
-                case 4:
+                case 4: mostrarVacas();
                     break;
                 case 5:
                     break; 
@@ -72,13 +73,15 @@ public class GestionVacas {
                if(nombreVaca[i] == ""){
                    indice = i;
                    
-                   //en el momento que usted encuentre ese esapcio vacio termine el proceso
+                   //en el momento que usted encuentre ese espacio vacio termine el proceso
                    break;
                    
                }
            }
         return indice;
        }
+     
+     
     public static void registrarVacas(){
         int indice = siguienteVaca();
         
@@ -87,9 +90,30 @@ public class GestionVacas {
         System.out.println("-----------------------------------------");
         
         leer.nextLine();
+        
+        System.out.println("Digite el nombre de la vaca");
         nombreVaca[indice] = leer.nextLine();
+      
+       
         
     }
+    
+     public static void mostrarVacas(){
+            //con el ciclo for mostramos los datos
+            
+            //no mostrar loq ue esta vacio
+                for(int i = 0; i <200; i++){
+                    
+                  /*  if(nombreHabitaciones[i]!= null){
+                       System.out.println("Habitaciones "+ (i+1) + " :"+ nombreHabitaciones[i]); 
+                    }*/
+                    
+                      if((nombreVaca[i]!= "")){
+                       System.out.println("Nombre de vaca:  "+ (i+1) + " :"+ nombreVaca[i]); 
+                    }
+                  
+                }
+       }
     
     
 }
