fa2e0690259d33b2037b66f43c6017b7eb9da93d
Erick <ericklopezborge21@gmail.com>
2026-07-12 19:04:49 -0600

    Agregando separaciones a los if o while ()

--- VaquitaFeliz/src/Logica/GestionAlimentos.java
+++ VaquitaFeliz/src/Logica/GestionAlimentos.java
@@ -152,7 +152,7 @@ public class GestionAlimentos {
  
         // Validacion de nombre duplicado usando equals()
         for (int i = 0; i < 100; i++) {
-            if (inventario[i][1] != "" && inventario[i][1].equals(nombre)) {
+            if (inventario[i][1].equals(nombre)) {
                 System.out.println("[!] Nombre existente en el registro");
                 return;
             }
